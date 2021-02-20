1a3137112aedf997dd58072c4854d930ebbe6649
ohthatbaguk <natashabaguk@outlook.com>
2021-02-20 17:57:30 +0300

    create first task (Stack);

---
+++ Homework_3/Collections/NewStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Homework_3.Collections
+{
+    public class NewStack
+    {
+        public Stack<int> CreateStack<T>()
+        {
+            Stack<int> stack = new Stack<int>();
+            Random random = new Random();
+            int MaxValue = 10;
+            int MinValue = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                stack.Push(random.Next(MinValue,MaxValue)); 
+            }
+            return stack;
+        }
+
+        public ArrayList CreateArrayWithDigits<T>(Stack<T> stack)
+        {
+            ArrayList digitsList = new ArrayList();
+            digitsList.AddRange(stack);
+            return digitsList;
+        }
+
+        public Stack<int> FindEqualElementsInStacks(Stack<int> firstStack, Stack<int> secondStack)
+        {
+            ArrayList firstList = CreateArrayWithDigits(firstStack);
+            ArrayList secondList = CreateArrayWithDigits(secondStack);
+            Stack<int> resultStack = new Stack<int>();
+            foreach (var digit in firstList)
+            {
+                if (secondList.Contains(digit))
+                {
+                    resultStack.Push((int) digit);
+                }
+            }
+
+            return resultStack;
+        }
+
+        public void PrintElementsInStack<T>(Stack<T> stack)
+        {
+            foreach (var element in stack)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Homework_3.Collections;
 
 namespace Homework_3
 {
@@ -7,45 +8,11 @@ namespace Homework_3
     {
         static void Main(string[] args)
         {
-            Stack<int> numbers1 = new Stack<int>();
-            Stack<int> numbers2 = new Stack<int>();
-            Stack<int> numbers3 = new Stack<int>();
-
-            Random random = new Random();
-            for (int i = 0; i < 10; i++)
-            {
-                numbers1.Push(random.Next(1, 20));
-                numbers2.Push(random.Next(1, 20));
-            }
-
-            foreach (var n in numbers1)
-            {
-                Console.Write($"{n} ");
-            }
-
-            Console.WriteLine();
-
-            foreach (var n in numbers2)
-            {
-                Console.Write($"{n} ");
-            }
-
-            while (numbers1.Count > 0 || numbers2.Count > 0)
-            {
-                if (numbers2.Count == 0 || numbers1.Count != 0)
-                    numbers3.Push(numbers1.Pop());
-                else
-                {
-                    numbers3.Push(numbers2.Pop());
-                }
-            }
-
-            Console.WriteLine();
-
-            foreach (var n in numbers3)
-            {
-                Console.Write($"{n} ");
-            }
+            NewStack newStack = new NewStack();
+            Stack<int> firstStack = newStack.CreateStack<int>();
+            Stack<int> secondStack = newStack.CreateStack<int>();
+            Stack<int> resultStack = newStack.FindEqualElementsInStacks(firstStack, secondStack);
+            newStack.PrintElementsInStack(resultStack);
         }
     }
 }
